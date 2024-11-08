e9ffef5a5efaddb5fc87fd688d950b751ec39c9f
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2024-11-08 13:00:48 +0500

    edition

--- src/Console/Commands/LaravelMakeViewCommand.php
+++ src/Console/Commands/LaravelMakeViewCommand.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Zintel\LaravelService\Console\Commands;
+namespace Zintel\LaravelViewMaker\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 
-class MakeViewCommand extends Command
+class LaravelMakeViewCommand extends Command
 {
     protected $signature = 'make:view {name} {--all} {--i} {--sh} {--u} {--e}';
     protected $description = 'Generate view files for a given section with optional flags for individual files.';
